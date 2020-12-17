b5c71a64d64dbdd95b031be25c27c88de42c88e4
pradeep20036 <pradeep20036@iiitd.ac.in>
2020-12-17 09:44:16 +0530

    adding a new print statement

--- src/main.java
+++ src/main.java
@@ -2,6 +2,7 @@ public class main {
 
     public static void main(String[] args) {
         System.out.println("hello world");
+        System.out.println("the world is a better place to live and survive");
     }
 
 }
